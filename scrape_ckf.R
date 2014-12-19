440c7a9c446f6eb0ca3082c6be0f5266bf6d39bc
Brian Loyal <bloyal@gmail.com>
2014-12-19 13:37:17 -0600

    keyword generation
    Completed draft function to generate menu item keywords

--- scrape_ckf.R
+++ scrape_ckf.R
@@ -21,7 +21,44 @@ getMenuItems <- function(){
   
   #Remove whitespace from descriptions
   ckf_items[,2]<-str_replace_all(ckf_items[,2],"[\t\r\n]","");
+  
+  #Remove cutoff words at end of sentences
+  ckf_items[,2]<-str_replace_all(ckf_items[,2],"\\s+\\w*\\W+$","")
+  
   #Remove duplicate records
-  ckf_items<-unique(ckf_items)
-  return ckf_items
+  ckf_items<-unique(ckf_items);
+  ckf_items;
+}
+
+#Process menu title and descriptions into keywords
+generateKeyWords <- function(menu){
+  library(tm);
+  
+  titles<-menu[,1];
+  descriptions<-menu[,2];
+  
+  #stem descriptions
+  #descriptions<-stemDocument(descriptions);
+  
+  #convert to lower case
+  titles<-tolower(titles);
+  descriptions<-tolower(descriptions);
+  
+  #remove IP symbols from titles
+  titles<-str_replace_all(titles,"[™®]","");
+  
+  #append titles to front of descriptions to make key word list
+  keyWordList<-paste(titles,descriptions);
+  
+  #Remove punctuation
+  keyWordList<-removePunctuation(keyWordList);
+  
+  #Remove stop words
+  stopWords <- readLines(system.file("stopwords", "english.dat",package = "tm"));
+  keyWordList<-removeWords(keyWordList,stopWords);
+
+  #Stem and Tokenize
+  keyWordList<-lapply(keyWordList,scan_tokenizer);
+  keyWordList<-lapply(keyWordList,unique);
+  keyWordList;
 }
