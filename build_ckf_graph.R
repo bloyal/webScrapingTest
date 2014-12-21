6eea98ae9427def3a7e6cd00fb0869d2685dd63e
Brian Loyal <bloyal@gmail.com>
2014-12-21 16:19:41 -0600

    Playing with automated node creation

--- build_ckf_graph.R
+++ build_ckf_graph.R
@@ -5,49 +5,64 @@ graph = startGraph("http://localhost:7474/db/data/");
 graph$version;
 # [1] "2.1.6"
 
-for (i in menu[,1]) {print(i)};
-
-#create menu item nodes
-for (i in menu[,1]) {
-  createNode(graph, "Item", name = i);
-};
-
-#add index 
-addIndex(graph, "Item", "name");
-
-#create feature nodes
-for (i in unique(unlist(features))) {
-  createNode(graph, "Feature", name = i);
-};
-
-#add index 
-addIndex(graph, "Feature", "name");
-
-full <- cbind(menu, features);
-
-#create relationships between items and features
-#This needs work - probably need to use lapply, or something
-
-apply(full, 1, function(row){
-      title_tmp <- row[[1]];
-      #print(title_tmp);
-      for (feature_tmp in row[[3]]){
-        #print(feature_tmp);
-        cypher(graph, "CREATE (i:Item)-[r:HAS_FEATURE]->(f:Feature) WHERE i.name={items} AND f.name={features}", items=title_tmp, features=feature_tmp);
-      }
-    }
-  )
 
 
-for (i in full) {
-  i[,1]
-#  for (j in unlist(features[i])) {
- #   print(i)
-  #  print(j)
-    #cypher(graph, "CREATE (i:Item {name:'Beer'})-[r:HAS_FEATURE]->(f:Feature {name:'beer'})");
-  #}
+createBulkNodes <- function(Properties,labelName){
+  apply(Properties, 1, function(row){
+    #print(row[1]);
+    #print(row[2]);
+    #print(row[3]);
+    #typeof(row[1]);
+    createNode(graph, labelName, name=row[[1]], hair = row[[2]], age = row[[3]]);
+  });
+  
+  
 }
 
-for (j in features) {
-  print(unlist(j))
-}
+# 
+# for (i in menu[,1]) {print(i)};
+# 
+# #create menu item nodes
+# for (i in menu[,1]) {
+#   createNode(graph, "Item", name = i);
+# };
+# 
+# #add index 
+# addIndex(graph, "Item", "name");
+# 
+# #create feature nodes
+# for (i in unique(unlist(features))) {
+#   createNode(graph, "Feature", name = i);
+# };
+# 
+# #add index 
+# addIndex(graph, "Feature", "name");
+# 
+# full <- cbind(menu, features);
+# 
+# #create relationships between items and features
+# #This needs work - probably need to use lapply, or something
+# 
+# apply(full, 1, function(row){
+#       title_tmp <- row[[1]];
+#       #print(title_tmp);
+#       for (feature_tmp in row[[3]]){
+#         #print(feature_tmp);
+#         cypher(graph, "CREATE (i:Item)-[r:HAS_FEATURE]->(f:Feature) WHERE i.name={items} AND f.name={features}", items=title_tmp, features=feature_tmp);
+#       }
+#     }
+#   )
+# 
+# 
+# for (i in full) {
+#   i[,1]
+# #  for (j in unlist(features[i])) {
+#  #   print(i)
+#   #  print(j)
+#     #cypher(graph, "CREATE (i:Item {name:'Beer'})-[r:HAS_FEATURE]->(f:Feature {name:'beer'})");
+#   #}
+# }
+# 
+# for (j in features) {
+#   print(unlist(j))
+# }
