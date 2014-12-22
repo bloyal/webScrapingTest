04d0fd45457ce0c54df75acd6b6d084cc3ad1551
Brian Loyal <bloyal@gmail.com>
2014-12-22 10:44:16 -0600

    Added bulk create relationships

--- build_ckf_graph.R
+++ build_ckf_graph.R
@@ -25,7 +25,8 @@ bulkCreateNodes<-function(graph, labelName, indexName, indexValues){
   addIndex(graph, labelName, indexName);
 }
 
-bulkUpdateNodeProperties<-function(graph, labelNames, indexNames, indexValues, propertyNames, propertyValues){
+bulkUpdateNodeProperties<-function(graph, labelNames, indexNames, indexValues, 
+                                   propertyNames, propertyValues){
   nodeInfo <- cbind(labelName=labelNames, indexName=indexNames, 
                     indexValue=as.character(indexValues), 
                     propertyName=propertyNames, 
@@ -38,5 +39,29 @@ bulkUpdateNodeProperties<-function(graph, labelNames, indexNames, indexValues, p
     appendCypher(t, query);
   }
 
+  commit(t);
+}
+
+bulkCreateRelationships(graph, 
+                        rep("MenuItem",5), rep("name",5), menu$name[1:5],
+                        rep("Feature",5), rep("name",5), unique(unlist(features))[1:5],
+                        rep("HAS_FEATURE",5)
+                        )
+
+bulkCreateRelationships<-function(graph, 
+                                  startLabel, startIndexName, startIndexValue, 
+                                  endLabel, endIndexName, endIndexValue,
+                                  relationshipType){
+  relInfo <- cbind(startLabel, startIndexName, startIndexValue,
+                   endLabel, endIndexName, endIndexValue,
+                   relationshipType);
+  t <- newTransaction(graph);
+  for (i in 1:nrow(relInfo)){
+    query <- paste("MERGE (a:", relInfo[i,"startLabel"]," {", relInfo[i,"startIndexName"], ":'", relInfo[i,"startIndexValue"], "'}) ",
+                   "MERGE (b:", relInfo[i,"endLabel"]," {", relInfo[i,"endIndexName"], ":'", relInfo[i,"endIndexValue"], "'}) ",
+                   "CREATE (a)-[r:",relInfo[i,"relationshipType"],"]->(b)", sep="");
+    #print(query)
+    appendCypher(t, query);
+  }
   commit(t);
 }
